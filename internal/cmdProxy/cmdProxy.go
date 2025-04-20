ee2dcfdea36f7977a45931c9b7c2d12249701d8d
HuYang <hy.ccccr@gmail.com>
2025-04-21 02:35:27 +0800

    feat: add command proxy endpoint with handshake

--- internal/cmdProxy/cmdProxy.go
+++ internal/cmdProxy/cmdProxy.go
@@ -1,18 +1,28 @@
 package cmdproxy
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	zmq "github.com/pebbe/zmq4"
 )
 
-// StartCmdProxyReq is the request body for starting the command proxy
-type StartCmdProxyReq struct {
+// Endpoint is the request body for starting the command proxy
+type Endpoint struct {
 	Hostname string `json:"hostname" binding:"required"`
 	Port     uint   `json:"port" binding:"required"`
+}
+
+type HandshakeREP struct {
+	Response string `json:"response"`
+}
+
+type HandshakeREQ struct {
+	Request string `json:"request"`
 }
 
 // ReqClient is a client for sending requests to the Rep server
@@ -24,10 +34,16 @@ type ReqClient struct {
 	mutex    sync.Mutex
 }
 
+type CreateCmdProxyResponse struct {
+	ID       string `json:"id"`
+	Hostname string `json:"hostname"`
+	Port     uint   `json:"port"`
+}
+
 var (
-	reqClient     *ReqClient // global ReqClient instance
-	reqClientOnce sync.Once  // sync.Once for initializing reqClient
-	reqClientErr  error      // error from creating reqClient
+	reqClientOnce     sync.Once // sync.Once for initializing reqClient
+	reqClientMap      = make(map[string]*ReqClient)
+	reqClientMapMutex sync.RWMutex
 )
 
 // Create a ZeroMQ REQ client
@@ -49,11 +65,17 @@ func createREQ(hostname string, port uint) (*ReqClient, error) {
 		return nil, fmt.Errorf("failed to connect to server: %v", err)
 	}
 
-	s.Send("Hello", 0)
+	var request = HandshakeREQ{
+		Request: "Hello",
+	}
+	requestJson, _ := json.Marshal(request)
+	s.SendBytes(requestJson, 0)
 
-	msg, err := s.Recv(0)
+	msgJson, err := s.RecvBytes(0)
+	var msg HandshakeREP
+	json.Unmarshal([]byte(msgJson), &msg)
 
-	if err != nil && msg != "World" {
+	if err != nil || msg.Response != "World" {
 		return nil, fmt.Errorf("failed to receive message from server: %v", err)
 	}
 
@@ -66,21 +88,21 @@ func createREQ(hostname string, port uint) (*ReqClient, error) {
 }
 
 // Send a message to the server and return the response
-func (r *ReqClient) Send(msg []byte) (string, error) {
+func (r *ReqClient) Send(msg []byte) ([]byte, error) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 
 	_, err := r.socket.SendBytes(msg, 0)
 	if err != nil {
-		return "", fmt.Errorf("failed to send message: %v", err)
+		return nil, fmt.Errorf("failed to send message: %v", err)
 	}
 
-	result, err := r.socket.Recv(0)
+	response, err := r.socket.RecvBytes(0)
 	if err != nil {
-		return "", fmt.Errorf("failed to receive message: %v", err)
+		return nil, fmt.Errorf("failed to receive message: %v", err)
 	}
 
-	return result, nil
+	return response, nil
 }
 
 // Close the REQ client
@@ -102,34 +124,51 @@ func (r *ReqClient) Close() error {
 }
 
 // Create a REQ client
-func startCmdProxy(c *gin.Context) {
-	var req StartCmdProxyReq
-	if err := c.ShouldBindJSON(&req); err != nil {
+func createCmdProxy(c *gin.Context) {
+	var endpoint Endpoint
+
+	if err := c.ShouldBindJSON(&endpoint); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	var reqClient *ReqClient
+	var reqInitErr error
 	reqClientOnce.Do(func() {
-		reqClient, reqClientErr = createREQ(req.Hostname, req.Port)
+		reqClient, reqInitErr = createREQ(endpoint.Hostname, endpoint.Port)
 	})
 
-	if reqClientErr != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": reqClientErr.Error()})
+	if reqInitErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": reqInitErr.Error()})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"result": "Command proxy started"})
+	reqClientMapMutex.Lock()
+	id := uuid.New().String()
+	reqClientMap[id] = reqClient
+	reqClientMapMutex.Unlock()
+
+	var reqClientResponse = CreateCmdProxyResponse{
+		ID:       id,
+		Hostname: endpoint.Hostname,
+		Port:     endpoint.Port,
+	}
+
+	c.JSON(http.StatusCreated, reqClientResponse)
 }
 
 // Forward a command to the server and return the response
-func cmd(c *gin.Context) {
-	if reqClient == nil {
+func sendCmd(c *gin.Context) {
+	reqClientMapMutex.RLock()
+	reqClient, exist := reqClientMap[c.Param("id")]
+	reqClientMapMutex.RUnlock()
+
+	if !exist {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Command proxy not started"})
 		return
 	}
 
 	cmd, err := c.GetRawData()
-
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
 		return
@@ -141,10 +180,10 @@ func cmd(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"repsonse": result})
+	c.Data(http.StatusCreated, "application/json", result)
 }
 
 func RegisterRoutes(r *gin.Engine) {
-	r.POST("/StartCmdProxy", startCmdProxy)
-	r.POST("/Cmd", cmd)
+	r.POST("/cmds/proxies", createCmdProxy)
+	r.POST("/cmds/proxies/:id", sendCmd)
 }
