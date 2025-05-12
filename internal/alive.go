34397842942a0219406eb30317a3997d4bada187
HuYang <hy.ccccr@gmail.com>
2025-05-12 23:51:32 +0800

    feat: :egg: add more monkey sayings

--- internal/alive.go
+++ internal/alive.go
@@ -1,32 +1,66 @@
 package alive
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-var artMsg = `
- _______________
-< love && peace >
- ---------------
+var monkeySayings = []string{
+	"love && peace",
+	"bananas are tasty",
+	"go bananas!",
+	"ooh ooh ah ah",
+	"code more, sleep less",
+	"monkey see, monkey do",
+	"throw no banana",
+	"climb every mountain",
+	"eat sleep code repeat",
+}
+
+type Alive struct {
+	ArtMsg string `json:"message"`
+}
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func generateMonkeyArt(saying string) string {
+	// Calculate the length of the saying
+	lineLength := len(saying) + 2 // 2 spaces
+
+	// Generate the top and bottom lines
+	topLine := " " + strings.Repeat("_", lineLength)
+	bottomLine := " " + strings.Repeat("-", lineLength)
+
+	// Generate the monkey art with the saying
+	monkeyArt := fmt.Sprintf(`
+%s
+< %s >
+%s
     \
       .-"-.
     _/.-.-.\_
    ( ( o o ) )
     |/  "  \|
      \ .-. /
-     /` + "`" + `"""` + "`" + `\
+     /`+"`"+`"""`+"`"+`\
     /       \
-`
+`, topLine, saying, bottomLine)
 
-type Alive struct {
-	ArtMsg string `json:"message"`
+	return monkeyArt
 }
 
 func GetAlive(c *gin.Context) {
+	saying := monkeySayings[rand.Intn(len(monkeySayings))]
+
 	response := Alive{
-		ArtMsg: artMsg,
+		ArtMsg: generateMonkeyArt(saying),
 	}
 
 	c.JSON(http.StatusOK, response)
