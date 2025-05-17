563c0bf9dc2c11aa2d79fe11f178bdd2e607c5dc
HuYang <30764038+Ccccraz@users.noreply.github.com>
2025-05-17 21:13:48 +0800

    feat: ✨ add cobra to support command-line arguments such as: --version (#28)

--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"os"
-
-	alive "github.com/Ccccraz/cogmoteGO/internal"
-	"github.com/Ccccraz/cogmoteGO/internal/broadcast"
-	cmdproxy "github.com/Ccccraz/cogmoteGO/internal/cmdProxy"
-	"github.com/Ccccraz/cogmoteGO/internal/experiments"
-	"github.com/Ccccraz/cogmoteGO/internal/health"
-	"github.com/gin-gonic/gin"
+	"github.com/Ccccraz/cogmoteGO/cmd"
 )
 
 func main() {
-	if envMode := os.Getenv("GIN_MODE"); envMode == "" {
-		gin.SetMode(gin.ReleaseMode)
-	} else {
-		gin.SetMode(envMode)
-	}
-
-	r := gin.New()
-	r.Use(gin.Recovery())
-	r.UseH2C = true
-
-	if gin.Mode() == gin.DebugMode {
-		r.Use(gin.Logger())
-	}
-
-	broadcast.RegisterRoutes(r)
-	cmdproxy.RegisterRoutes(r)
-	health.RegisterRoutes(r)
-	alive.RegisterRoutes(r)
-	experiments.RegisterRoutes(r)
-
-	r.Run(":9012")
+	cmd.Execute()
 }
