6bab5eedc81c49261e2081162af741a361aa9c5f
HuYang <hy.ccccr@gmail.com>
2025-07-30 17:45:57 +0200

    feat: :sparkles: quickly reset systemd service

--- cmd/reset.go
+++ cmd/reset.go
@@ -1,40 +1,42 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/Ccccraz/cogmoteGO/internal/logger"
 	"github.com/spf13/cobra"
 )
 
 // resetCmd represents the reset command
 var resetCmd = &cobra.Command{
 	Use:   "reset",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "quickly reset the service",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("reset called")
+		service := createService()
+
+		var needreset bool
+
+		if err := service.Uninstall(); err != nil {
+			logger.Logger.Info(err.Error())
+		} else {
+			logger.Logger.Info("Service uninstalled")
+			needreset = true
+		}
+
+		if err := service.Install(); err != nil {
+			logger.Logger.Info(err.Error())
+		} else {
+			if needreset {
+				logger.Logger.Info("Service reinstalled")
+			} else {
+				logger.Logger.Info("Service installed")
+			}
+		}
+
 	},
 }
 
 func init() {
 	serviceCmd.AddCommand(resetCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// resetCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// resetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
